948051af98ccd31d2937af1a08268d8f392fb5ca
Nazarreto <102985652+Nazarreto@users.noreply.github.com>
2023-02-28 19:39:47 +0200

    create

---
+++ script.js
@@ -0,0 +1,102 @@
+// Task1
+
+// let ask = prompt(`Введіть пароль`)
+// let password = "myPassword"; 
+// let attempts = 4;
+
+// while(attempts>0){
+//   if(ask===password){
+//     alert("Запрошуємо на сайт!");
+//   }
+//   else if(ask!==password){
+//     ask = prompt(`Кількість спроб ${attempts}`)
+//   }
+//   attempts--
+// while(attempts<1){
+//     alert(`Будь ласка спробуйте пізніше!`)}
+    
+// }
+
+// task 2  
+
+// let n = +prompt('Введіть число')
+// let result = 1
+// let i =2
+// while(i <= n){
+//   result = result * i
+//   i++
+// }
+//   console.log(result);
+
+
+// task 3
+
+// let count = 1000
+// while(count<=9999){
+//   document.write(`<h2>Loop ${count}</h2>`)
+//   count+=3
+// }
+
+// task 4
+
+// let count = 1
+// while(count<=99){
+//   document.write(`<h2>Loop ${count}</h2>`)
+//   count+=2
+// }
+
+// task 5 
+
+// let count = 90 
+// while(count>=0){
+//   document.write(`<h2>Loop ${count}</h2>`)
+//   count-=5
+// }
+
+// task 6
+
+// let count = 2 
+// while(count<=1119999){
+//   document.write(`<h2>Loop ${count}</h2>`)
+//   count*= 2
+// }
+
+// task 7 
+
+
+// let a = 2
+// while(a<=10000){
+//   document.write(`<h2>Loop ${a}</h2>`)
+//   a=2*a-1
+// }
+
+// task 8
+
+
+// let a = -166;
+// while (2*a+200 < 100) {
+//   if (2*a+200 > 9 && 2*a+200 < 100) {
+//     console.log(2*a+200);
+//   }
+//   a++;
+// }
+
+
+// task 9 
+
+
+// let a = prompt("Введіть число: ");
+// let b = prompt("Введіть степінь: ");
+// let result = 1;
+// let i = 1
+// while(i<=Math.abs(b)){
+//   result*=a
+//   i++
+// }
+// if (b < 0){
+//   result = 1 / result
+// }
+// else if (b===0){
+//   result = 1
+// }
+// alert(`${a} в степені ${b} дорівнює ${result}`);
